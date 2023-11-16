780c29786c8dfc129644cea15b49e8a080c21903
Maitreya-today <110085807+Maitreya-today@users.noreply.github.com>
2023-11-15 22:17:18 -0600

    update ai

--- pong-one/Assets/Scripts/AIPlayer.cs
+++ pong-one/Assets/Scripts/AIPlayer.cs
@@ -4,10 +4,25 @@ public class AIPlayer : MonoBehaviour
 {
     private Paddle paddle;  // Assuming your player paddle script is named Paddle.cs
 
+    public float difficultyFactor = 1.0f;  // Adjust this based on difficulty settings
+
     void Start()
     {
         FindPlayerPaddle();
         DisableIfPlayerExists();
+    }
+
+    void FixedUpdate()
+    {
+        // Track the ball's Y position
+        float targetY = Ball.Instance.transform.position.y;
+
+        // Adjust the paddle's Y position based on the ball's position with some randomness
+        float yOffset = Random.Range(-0.5f, 0.5f) * difficultyFactor;
+        float newY = Mathf.Lerp(transform.position.y, targetY + yOffset, Time.deltaTime * difficultyFactor);
+
+        // Apply the new Y position to the paddle
+        transform.position = new Vector2(transform.position.x, newY);
     }
 
     void FindPlayerPaddle()
@@ -19,20 +34,5 @@ public class AIPlayer : MonoBehaviour
     void DisableIfPlayerExists()
     {
         // Check if the player script is present in the parent object
-        Player player = paddle.GetComponent<Player>();
-        
-        if (player != null)
-        {
-            // If player script is present, disable it
-            player.enabled = false;
-        }
-    }
+        Player player
 
-    // Add your AI logic for paddle movement here
-
-    // You might want to use FixedUpdate for physics-related updates
-    void FixedUpdate()
-    {
-        // Add your AI logic for paddle movement based on ball position
-    }
-}
