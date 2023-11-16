d698706dbab53bebd2fc0e47799d30b94762ea6b
Maitreya-today <110085807+Maitreya-today@users.noreply.github.com>
2023-11-15 21:58:26 -0600

    player 2 and game script

--- pong-one/Assets/Scripts/Ball.cs
+++ pong-one/Assets/Scripts/Ball.cs
@@ -1,163 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
-namespace ZPong
+
+public class Ball : MonoBehaviour
 {
+    public float speed = 5f;
+    private Vector2 direction;
 
-    public class Ball : MonoBehaviour
+    void Start()
     {
-        public float speed = 5f;
+        // Initialize the ball's direction
+        direction = new Vector2(1, 0).normalized;
+    }
 
-        private float screenTop = 527;
-        private float screenBottom = -527;
+    void Update()
+    {
+        // Move the ball
+        transform.Translate(direction * speed * Time.deltaTime);
 
-        private Vector2 direction;
-        private Vector2 defaultDirection;
+        // Check for collisions with paddles
+        CheckPaddleCollision();
+    }
 
-        private bool ballActive;
+    void CheckPaddleCollision()
+    {
+        // Use this to check for collisions with the paddles
+        // You can replace "Player One" and "Player Two" with your actual paddle GameObject names
+        GameObject playerOne = GameObject.Find("Player One");
+        GameObject playerTwo = GameObject.Find("Player Two");
 
-        protected RectTransform rectTransform;
-
-        private AudioSource bounceSFX;
-
-
-        private void Start()
+        if (playerOne != null && playerTwo != null)
         {
-            rectTransform = GetComponent<RectTransform>();
+            Collider2D playerOneCollider = playerOne.GetComponent<Collider2D>();
+            Collider2D playerTwoCollider = playerTwo.GetComponent<Collider2D>();
 
-            if (PlayerPrefs.HasKey("BallSpeed"))
+            if (playerOneCollider.bounds.Intersects(GetComponent<Collider2D>().bounds))
             {
-                speed = PlayerPrefs.GetFloat("BallSpeed");
+                // Calculate rebound angle based on the sweet spot effect
+                float hitPoint = transform.position.y - playerOne.transform.position.y;
+                float normalizedHitPoint = hitPoint / (playerOneCollider.bounds.size.y / 2f);
+
+                direction = new Vector2(1, normalizedHitPoint).normalized;
             }
-
-            if (PlayerPrefs.HasKey("BallSize"))
+            else if (playerTwoCollider.bounds.Intersects(GetComponent<Collider2D>().bounds))
             {
-                var value = PlayerPrefs.GetFloat("BallSize");
-                rectTransform.sizeDelta = new Vector2(value, value);
-            }
+                // Similar calculations for Player Two's paddle
+                float hitPoint = transform.position.y - playerTwo.transform.position.y;
+                float normalizedHitPoint = hitPoint / (playerTwoCollider.bounds.size.y / 2f);
 
-            if (PlayerPrefs.HasKey("PitchDirection"))
-            {
-                string pitchDirectionValue = PlayerPrefs.GetString("PitchDirection");
-    
-                if (pitchDirectionValue == "Random")
-                {
-                    // Generate a random direction between -1 and 1 for the x-axis.
-                    float randomX = Random.Range(-1f, 1f);
-                    direction = new Vector2(randomX, 0f).normalized;
-                }
-                else if (pitchDirectionValue == "Right")
-                {
-                    // Set the direction to move right.
-                    direction = new Vector2(1f, 0f);
-                }
-                else
-                {
-                    // Default to moving left if the value is not recognized.
-                    direction = new Vector2(-1f, 0f);
-                }
-            }
-            else
-            {
-                // Default to moving left if no value is found in PlayerPrefs.
-                direction = new Vector2(-1f, 0f);
-            }
-
-            defaultDirection = direction;
-
-            SetHeightBounds();
-
-            bounceSFX = this.GetComponent<AudioSource>();
-        }
-
-        private void Update()
-        {
-            if (ballActive)
-            {
-
-
-                Vector2 newPosition = rectTransform.anchoredPosition + (direction * speed * Time.deltaTime);
-
-                rectTransform.anchoredPosition = newPosition;
-
-
-                if (rectTransform.anchoredPosition.y >= screenTop || rectTransform.anchoredPosition.y <= screenBottom)
-                {
-                    direction.y *= -1f;
-                    PlayBounceSound();
-                }
+                direction = new Vector2(-1, normalizedHitPoint).normalized;
             }
         }
-
-        protected void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.gameObject.CompareTag("Paddle"))
-            {
-                Paddle paddle = collision.gameObject.GetComponent<Paddle>();
-
-                float y = BallHitPaddleWhere(GetPosition(), paddle.AnchorPos(),
-                    paddle.GetComponent<RectTransform>().sizeDelta.y / 2f);
-                Vector2 newDirection = new Vector2(paddle.isLeftPaddle ? 1f : -1f, y);
-
-                Reflect(newDirection);
-                PlayBounceSound();
-            }
-            else if (collision.gameObject.CompareTag("Goal"))
-            {
-                // Debug.Log("pos: " + rectTransform.anchoredPosition.x);
-                //Left goal
-                if (this.rectTransform.anchoredPosition.x < -1)
-                {
-                    ScoreManager.Instance.ScorePointPlayer2();
-                }
-                //Right goal
-                else
-                {
-                    ScoreManager.Instance.ScorePointPlayer1();
-                }
-            }
-        }
-
-        public void Reflect(Vector2 newDirection)
-        {
-            direction = newDirection.normalized;
-        }
-
-        public void SetBallActive(bool value)
-        {
-            ballActive = value;
-            direction = defaultDirection;
-        }
-
-        public Vector2 GetPosition()
-        {
-            return rectTransform.anchoredPosition;
-        }
-
-        public void SetHeightBounds()
-        {
-            var height = UIScaler.Instance.GetUIHeightPadded();
-
-            screenTop = height / 2;
-            screenBottom = -1 * height / 2;
-        }
-
-        protected float BallHitPaddleWhere(Vector2 ball, Vector2 paddle, float paddleHeight)
-        {
-            return (ball.y - paddle.y) / paddleHeight;
-        }
-
-        void PlayBounceSound()
-        {
-            bounceSFX.pitch = Random.Range(.8f, 1.2f);
-            bounceSFX.Play();
-        }
-        
-        public void DisableBall()
-        {
-            ballActive = false;
-        }
-
     }
 }
---
+++ pong-one/Assets/Scripts/PongGame.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PongGame : MonoBehaviour
+{
+    public int winningScore = 11;
+    public Text victoryMessage;
+    public Text player1ScoreText;
+    public Text player2ScoreText;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
+
+    void Start()
+    {
+        // Initialize UI text components
+        victoryMessage.text = "";
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        // Update the UI with current scores
+        player1ScoreText.text = "Player 1: " + player1Score.ToString();
+        player2ScoreText.text = "Player 2: " + player2Score.ToString();
+    }
+
+    void PlayerScored(int playerNumber)
+    {
+        if (playerNumber == 1)
+        {
+            player1Score++;
+        }
+        else if (playerNumber == 2)
+        {
+            player2Score++;
+        }
+
+        UpdateScoreUI();
+
+        // Check if either player has reached the winning score
+        if (player1Score >= winningScore || player2Score >= winningScore)
+        {
+            DeclareWinner();
+        }
+    }
+
+    void DeclareWinner()
+    {
+        // Stop the game (pause or disable player controls)
+
+        // Display victory and defeat messages
+        if (player1Score >= winningScore)
+        {
+            victoryMessage.text = "Player 1 Wins!";
+        }
+        else
+        {
+            victoryMessage.text = "Player 2 Wins!";
+        }
+    }
+
+    // Add your other game logic, input handling, and ball movement here
+
+    // Example usage (call this method when a player scores a point)
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            // Determine which player scored based on the ball's position
+            int playerNumber = (collision.gameObject.transform.position.x > 0) ? 1 : 2;
+            PlayerScored(playerNumber);
+        }
+    }
+}
