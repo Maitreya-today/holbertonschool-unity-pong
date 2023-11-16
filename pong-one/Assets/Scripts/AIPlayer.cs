f1966ad84740d6e5882cbfb52d5dd634e78d9f3d
Maitreya-today <110085807+Maitreya-today@users.noreply.github.com>
2023-11-15 22:20:10 -0600

    documentation

--- pong-one/Assets/Scripts/AIPlayer.cs
+++ pong-one/Assets/Scripts/AIPlayer.cs
@@ -2,37 +2,38 @@ using UnityEngine;
 
 public class AIPlayer : MonoBehaviour
 {
-    private Paddle paddle;  // Assuming your player paddle script is named Paddle.cs
+    private Paddle paddle;  // Reference to the player paddle script
 
     public float difficultyFactor = 1.0f;  // Adjust this based on difficulty settings
 
     void Start()
     {
+        // Find and store the reference to the player paddle
         FindPlayerPaddle();
+        
+        // Disable the player script if it exists in the parent object
         DisableIfPlayerExists();
     }
 
     void FixedUpdate()
     {
-        // Track the ball's Y position
+        // Track the ball's Y position for AI movement
         float targetY = Ball.Instance.transform.position.y;
 
         // Adjust the paddle's Y position based on the ball's position with some randomness
         float yOffset = Random.Range(-0.5f, 0.5f) * difficultyFactor;
         float newY = Mathf.Lerp(transform.position.y, targetY + yOffset, Time.deltaTime * difficultyFactor);
 
-        // Apply the new Y position to the paddle
+        // Apply the new Y position to the AI-controlled paddle
         transform.position = new Vector2(transform.position.x, newY);
     }
 
+    // Finds the player paddle script in the parent object
     void FindPlayerPaddle()
     {
-        // Find the player paddle in the parent object
         paddle = GetComponentInParent<Paddle>();
     }
 
+    // Disables the player script if it exists in the parent object
     void DisableIfPlayerExists()
     {
-        // Check if the player script is present in the parent object
-        Player player
-
